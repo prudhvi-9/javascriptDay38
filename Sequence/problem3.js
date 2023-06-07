25fb67cf68a41b485f7249d18adc5f771c7b14ea
PRUDHVI-9 <prudhvirajathelli@gmail.com>
2023-06-07 09:12:35 +0530

    Addeed:To add two dice number using random numbers.

---
+++ Sequence/problem3.js
@@ -0,0 +1,9 @@
+// 3: Add 2 random dice numbers and check sum
+let number1 = Math.floor(Math.random() * 6 +1);
+console.log("Number1:"+number1);
+
+let number2 = Math.floor(Math.random() * 6 +1);
+console.log("Number2:"+number2);
+
+let sum = number1 + number2;
+console.log("Sum of two numbers : " + sum);
