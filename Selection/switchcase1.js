8add4af7eab20037801b336dc481d820b9191a0b
PRUDHVI-9 <prudhvirajathelli@gmail.com>
2023-06-07 09:34:09 +0530

    Addeed:To write the number in word using Case .

---
+++ Selection/switchcase1.js
@@ -0,0 +1,35 @@
+//Read a single digit number and write the number in word using Switch Case
+
+let number = Number(prompt("Enter a single digit number : "));
+
+switch (number) {
+    case 0:
+        console.log("Number is Zero");
+        break;
+    case 1:
+        console.log("Number is One");
+        break;
+    case 2:
+        console.log("Number is Two");
+        break;
+    case 3:
+        console.log("Number is Three");
+        break;
+    case 5:
+        console.log("Number is Five");
+        break;
+    case 6:
+        console.log("Number is Six");
+        break;
+    case 7:
+        console.log("Number is Seven");
+        break;
+    case 8:
+        console.log("Number is Eight");
+        break;
+    case 9:
+        console.log("Number is Nine");
+        break;
+    default:
+        console.log("Invalid");
+}     
